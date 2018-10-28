edb1a096a022ee7d1f45b48d4b3732e7e7c4dca4
venkatnarasimharao <38693729+venkatnarasimharao@users.noreply.github.com>
2018-10-28 13:14:39 +0530

    pop

--- 09_Angular_ng_show_ng_hide/09_app.js
+++
@@ -1,9 +0,0 @@
-//creation of module
-var app=angular.module('HobbiesApp',[]);
-
-//creation of controller
-app.controller('HobbiesCtrlApp',function() {
-    this.eating = false;
-    this.coading = false;
-    this.sleeping = false;
-});
