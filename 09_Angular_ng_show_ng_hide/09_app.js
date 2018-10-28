b70fa27ee9b5e666f04e22d3f4034980c7fcc2dc
venkatnarasimharao <38693729+venkatnarasimharao@users.noreply.github.com>
2018-10-28 13:15:37 +0530

    push

---
+++ 09_Angular_ng_show_ng_hide/09_app.js
@@ -0,0 +1,9 @@
+//creation of module
+var app=angular.module('HobbiesApp',[]);
+
+//creation of controller
+app.controller('HobbiesCtrlApp',function() {
+    this.eating = false;
+    this.coading = false;
+    this.sleeping = false;
+});
